8c3b7e28b23522b25eb081983893afcf39621427
Christian Kossi <chrishouns21@gmail.com>
2023-02-08 09:30:36 +0100

    orm file updated

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -2,7 +2,7 @@ import { ConnectionOptions } from 'typeorm';
 
 const ormconfig: ConnectionOptions = {
   type: 'postgres',
-  host: 'host.docker.internal',
+  host: 'db',
   port: 5432,
   username: 'postgres',
   password: 'postgres',
