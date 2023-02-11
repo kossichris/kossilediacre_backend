2b180310f7909090dfaa6e01ce447f78a1190ac0
Christian Kossi <chrishouns21@gmail.com>
2023-02-11 16:07:20 +0100

    orm config

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -2,7 +2,7 @@ import { ConnectionOptions } from 'typeorm';
 
 const ormconfig: ConnectionOptions = {
   type: 'postgres',
-  host: 'postgresql',
+  host: '0.0.0.0',
   port: 5432,
   username: 'postgres',
   password: 'postgres',
